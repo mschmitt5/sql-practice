1fa503a0c7a8b2dd9c2f35370e342813f60235e8
Mary <schmitt.mary7@gmail.com>
2018-01-17 10:13:17 -0700

    updated UUID input

--- practice.sql
+++ practice.sql
@@ -18,7 +18,7 @@ CREATE TABLE profile (
 CREATE TABLE article (
   articleId BINARY(16) NOT NULL,
   articleProfileId BINARY(16) NOT NULL,
-  articleText VARCHAR(65535) NOT NULL,
+  articleText VARCHAR(21000) NOT NULL,
   articleTitle VARCHAR(128) NOT NULL,
   INDEX (articleProfileId),
   FOREIGN KEY(articleProfileId) REFERENCES profile(profileId),
@@ -37,15 +37,16 @@ CREATE TABLE clap(
 );
 
 INSERT INTO profile (profileId, profileEmail, profileHash, profileName, profileSalt, profileStatement)
-VALUES (UNHEX(REPLACE("f135186a-f29e-41b4-84bf-504b1f008d4b")), "schmitt.mary7@gmail.com", unhex(replace("7e321d5d-0e80-4a79-8106-05f737b1fd6a")), "Mary Schmitt", unhex(replace("903a5a88-b04d-439e-8aa1-a1c370e60ef3
-")), "Here is a string. My author profile."
+VALUES (UNHEX(REPLACE("f135186a-f29e-41b4-84bf-504b1f008d4b", "-", "")), "schmitt.mary7@gmail.com", unhex(replace("7e321d5d-0e80-4a79-8106-05f737b1fd6a", "-", "")), "Mary Schmitt", unhex(replace("903a5a88-b04d-439e-8aa1-a1c370e60ef3
+", "-", "")), "Here is a string. My author profile."
 );
 
 INSERT INTO article (articleId, articleProfileId, articleText, articleTitle) VALUES (unhex(replace("cb228c4c-855e-4dc0-ae5f-e0b7a3ee4fb6
-")), unhex(replace("f135186a-f29e-41b4-84bf-504b1f008d4b")), "Another string. What a short article.", "String. Title is as long as the article"
+", "-", "")), unhex(replace("f135186a-f29e-41b4-84bf-504b1f008d4b", "-", "")), "Another string. What a short article.", "String. Title is as long as the article"
 );
 
-INSERT INTO clap (clapId, clapArticleId, clapProfileId) VALUES (unhex(replace("8c08c76e-318c-40a7-bf3b-c8bf2795bb3b")), unhex(replace("cb228c4c-855e-4dc0-ae5f-e0b7a3ee4fb6")), unhex(replace("f135186a-f29e-41b4-84bf-504b1f008d4b")));
+INSERT INTO clap (clapId, clapArticleId, clapProfileId) VALUES (unhex(replace("8c08c76e-318c-40a7-bf3b-c8bf2795bb3b", "-", "")), unhex(replace("cb228c4c-855e-4dc0-ae5f-e0b7a3ee4fb6", "-", "")), unhex(replace("f135186a-f29e-41b4-84bf-504b1f008d4b", "-", ""))
+);
 
 UPDATE profile
 SET profileEmail = "macmillan.mary7@gmail.com", profileName = "Mary MacMillan"
@@ -56,8 +57,7 @@ SET articleTitle = "Short Title"
 WHERE articleId = "cb228c4c-855e-4dc0-ae5f-e0b7a3ee4fb6";
 
 DELETE FROM article
-WHERE articleId = "cb228c4c-855e-4dc0-ae5f-e0b7a3ee4fb6
-";
+WHERE articleId = "cb228c4c-855e-4dc0-ae5f-e0b7a3ee4fb6";
 
 DELETE FROM profile
 WHERE profileId = "f135186a-f29e-41b4-84bf-504b1f008d4b";
